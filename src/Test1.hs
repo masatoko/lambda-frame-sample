74e1311a394609a3bb497d726f79eabe9b58e668
masatoko <masatokodo@gmail.com>
2018-02-07 17:41:09 +0900

    Modify comments

--- src/Test1.hs
+++ src/Test1.hs
@@ -1,22 +1,23 @@
 module Test1 where
 
-import           Control.Monad       (forM_, when)
-import           Control.Monad.Extra (whenJust, whenJustM)
-import qualified Data.IntMap         as IM -- 整数専用のマップ
+import           Control.Monad       (forM_, when)         -- https://hackage.haskell.org/package/base/docs/Control-Monad.html
+import           Control.Monad.Extra (whenJust, whenJustM) -- https://hackage.haskell.org/package/extra/docs/Control-Monad-Extra.html
+import qualified Data.IntMap         as IM                 -- 整数専用のマップ https://hackage.haskell.org/package/containers/docs/Data-IntMap.html
 
 import           Script              (ButtonState (..), JetOperation (..),
                                       JoystickEvent (..), Script)
 import qualified Script              as S -- ゲームのコマンド
 
--- このscript関数がフレームごとに呼ばれる。
+-- 毎時刻、このscript関数が呼ばれる。
+-- その時刻に、君のロボットをどう動かすかを、この関数の中に書こう。
 script :: Script ()
 script = do
-  -- '--'で始まる行はコメント扱い
-  -- ここにある関数が、上から順に実行される。
-  t <- S.getTime -- フレーム時刻を得る
-  when (t == 0) $ do
-    -- 最初の時刻の場合ここが実行される
-    -- putStr_ "メッセージ" は画面上にメッセージを表示する
+  -- '--'で始まる行はコメント扱いとなる。
+  -- ここから命令が沢山書いてある、上から順に実行されるよ。
+  t <- S.getTime -- 時刻を得る。時刻は整数値で、最初は0、次の時刻では1と、1ずつ進む。
+  when (t == 0) $ do -- 最初の時刻の場合
+    -- when (何か) で、その 何か が真(True)の場合、ここが実行される。
+    -- putStr_ "メッセージ" は画面上にメッセージを表示。
     S.putStr_ "[RB]:      撃つ"
     S.putStr_ "[X][Y]:    変形"
     S.putStr_ "[LT]:      左ジェット"
@@ -25,19 +26,19 @@ script = do
 
   jes <- S.getJoystickEvents -- ジョイスティックのイベントを得る。どのボタンが押されているかのイベント。
   -- putStr_ $ show jes
-  axes <- S.getJoyAxes -- ジョイスティックの軸の状態（スティックの傾き・トリガーの押し具合）を得る
+  axes <- S.getJoyAxes -- ジョイスティックの軸の状態（スティックの傾き・トリガーの押し具合）を得る。
   -- putStr_ $ show axes
 
   -- ** Gun
   when (S.JoystickEvent 5 S.Pressed `elem` jes) $ do -- ボタン５が押されているとき
-    S.shootGun "gun-l1" -- gun-l1 の名前のGunから弾を撃つ
+    S.shootGun "gun-l1" -- gun-l1 の名前のGunから弾を撃つ。
     S.shootGun "gun-l2"
     S.shootGun "gun-r1"
     S.shootGun "gun-r2"
   --
   -- ** Wheel
-  whenJust (IM.lookup 0 axes) $ \d -> -- 軸０の値（スティックの傾き）を探す
-    when (abs d > 0.2) $ do -- 絶対値が指定値より大きい場合は（dは -1 以上 1 以下の値をとる）
+  whenJust (IM.lookup 0 axes) $ \d -> -- 軸０の値（スティックの傾き）を探す。
+    when (abs d > 0.2) $ do -- 絶対値が指定値より大きい場合（dは -1 以上 1 以下の値をとる）
       let work name = S.addRotImpulse name (-4 * d) -- name の名前のホイールを回転させる関数を定義する。正の値は時計回り。
       work "wheel-l1"
       work "wheel-l2"
